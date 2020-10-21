1726c4eec71fe27901a9824e2933f3a21c209731
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-10-21 07:50:02 -0700

    demo complete

--- my_code.py
+++ my_code.py
@@ -1,16 +1,17 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-def inc(x):
-    # User code goes here
-    return x + 1
+pets = {}
 
+while True:
+    key = input("Enter the name of a pet. ")
+    key = key.strip()
+    value = input(f"What kind of pet is/was {key}?")
+    value = value.strip()
+    pets[key] = value
+    again = input("Enter another? Press any key to keep continue adding pets. Type 'q' to quit. ")
+    if again == 'q':
+        break
 
-if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(inc(6))
+print(pets)
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
